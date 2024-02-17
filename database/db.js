9f7afefc64b1d38b3982063c8ee80f7882cac808
bimalkishore <erbimalhimanshu@gamil.com>
2024-02-17 15:27:07 +0530

    sapce removed

--- database/db.js
+++ database/db.js
@@ -3,7 +3,6 @@ const mongoString = process.env.DATABASE_URL;
 
 mongoose.connect(mongoString);
 const database = mongoose.connection;
-
 database.on('error', (error) => {
     console.log(error)
 })
